689ce384baea7265897ec07e4a63c15c7319d3e5
harilal04 <harilalc66@gmail.com>
2023-01-01 23:57:24 +0530

    added booking tickets but still pending some work

--- src/REPOSITORY/booking-repository.js
+++ src/REPOSITORY/booking-repository.js
@@ -4,6 +4,7 @@ const { Booking } = require('../MODELS/index');
 const { AppError, ValidationError } = require('../UTILS/ERRORS/index');
 
 class BookingRepository {
+    
     async create(data) {
         try {
             const booking = await Booking.create(data);
@@ -16,6 +17,23 @@ class BookingRepository {
                 'RepositoryError', 
                 'Cannot create Booking', 
                 'There was some issue creating the booking, please try again later',
+                StatusCodes.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    async update(bookingId, data) {
+        try {
+            const booking = await Booking.findByPk(bookingId);
+            if(data.status) {
+                booking.status = data.status;
+            }
+            await booking.save();
+            return booking;
+        } catch (error) {
+            throw new AppError(
+                'RepositoryError', 
+                'Cannot update Booking', 
+                'There was some issue updating the booking, please try again later',
                 StatusCodes.INTERNAL_SERVER_ERROR);
         }
     }
